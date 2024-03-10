d2dbc405530b0e39f2d932439628f282b7898dce
RecepCelik1 <133972905+RecepCelik1@users.noreply.github.com>
2024-03-10 15:37:18 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -42,22 +42,19 @@ function App() {
   function calculateLoanPaymentsWithExtraPayment(loanAmount, annualInterestRate, loanTermInYears, monthlyExtraPayment) {
     const monthlyInterestRate = annualInterestRate / 12 / 100;
     const numberOfPayments = loanTermInYears * 12;
-  
 
     const monthlyPayment =
       (loanAmount * monthlyInterestRate) /
-      (1 - Math.pow(1 + monthlyInterestRate, -numberOfPayments)) +
-      monthlyExtraPayment;
-  
+      (1 - Math.pow(1 + monthlyInterestRate, -numberOfPayments));
 
     let paymentSchedule = [];
     let remainingLoanAmount = loanAmount;
-  
-    for (let i = 1; i <= numberOfPayments; i++) {
+
+    for (let i = 1; i <= numberOfPayments && remainingLoanAmount > 0; i++) {
       const interestPayment = remainingLoanAmount * monthlyInterestRate;
       const principalPayment = monthlyPayment - interestPayment;
       const totalPayment = monthlyPayment + monthlyExtraPayment;
-  
+
       paymentSchedule.push({
         month: i,
         monthlyPayment: totalPayment.toFixed(2),
@@ -65,20 +62,25 @@ function App() {
         principalPayment: principalPayment.toFixed(2),
         remainingLoanAmount: remainingLoanAmount.toFixed(2),
       });
-  
-      remainingLoanAmount -= principalPayment;
+
+      remainingLoanAmount -= principalPayment + monthlyExtraPayment;
+
+      if (remainingLoanAmount <= 0) {
+        remainingLoanAmount = 0;
+        break; // Borç bittiğinde döngüyü sonlandır
+      }
     }
-  
+
     const totalInterestPaid = paymentSchedule.reduce((total, payment) => total + parseFloat(payment.interestPayment), 0);
     const totalAmountPaid = totalInterestPaid + loanAmount;
-  
+
     return {
       paymentSchedule,
       totalInterestPaid: totalInterestPaid.toFixed(2),
       totalAmountPaid: totalAmountPaid.toFixed(2),
       remainingLoanAmount: remainingLoanAmount.toFixed(2),
     };
-  }
+}
 
   const Calculate = () => {
     setCalculateResults(true)
@@ -93,8 +95,10 @@ function App() {
     setResults(result)
   }  
 
+  console.log(results)
 
-
+  let actualTermInYears = (results.paymentSchedule.length)/12
+  let averageInterestPayment = (results.totalInterestPaid)/(results.paymentSchedule.length)
 
   return (
     <div className="h-full overflow-auto">
@@ -137,6 +141,13 @@ function App() {
             />
           </div>
           <div className="mt-2 w-full">
+            <input 
+              className="h-12 p-2 rounded-md w-full"
+              onChange={(e) => parsingFunction(e.target.value , "extraPayments")}
+              placeholder="Extra monthly payment"
+            />
+          </div>
+          <div className="mt-2 w-full">
 
           </div>
 
@@ -160,21 +171,34 @@ function App() {
             <div className='border-gray-400 border'></div>  
 
             <div className="flex flex-col items-center mt-2">
+
               <div className="flex justify-between w-full">
                 <div className="flex justify-center items-center font-semibold ">MONTHLY PAYMENT</div>
-                <div className="font-semibold text-xl flex justify-center items-center">{results?.paymentSchedule[0]?.monthlyPayment}$</div>
+                <div className="flex justify-center items-center font-sans text-lg">{results?.paymentSchedule[0]?.monthlyPayment}$</div>
               </div>
+
+              <div className="flex justify-between w-full mt-2">
+                <div className="flex justify-center items-center font-semibold ">AVERAGE MONTHLY INTEREST</div>
+                <div className="flex justify-center items-center font-sans text-lg">{averageInterestPayment.toFixed(2)}$</div>
+              </div>
+
               <div className="flex justify-between w-full mt-2">
                 <div className="flex justify-center items-center font-semibold ">TOTAL INTEREST</div>
-                <div className="font-semibold text-xl flex justify-center items-center">{results?.totalInterestPaid}$</div>
+                <div className="flex justify-center items-center font-sans text-lg">{results?.totalInterestPaid}$</div>
               </div>
+
+              <div className="flex justify-between w-full mt-2">
+                <div className="flex justify-center items-center font-semibold ">NUMBER OF YEARS</div>
+                <div className="flex justify-center items-center font-sans text-lg">{actualTermInYears.toFixed(2)}$</div>
+              </div>
+
             </div>
 
             <div className="w-full bg-emerald-900 text-sm flex justify-between text-white rounded-md mt-2">
               <div className="font-bold justify-center items-center w-24 p-4">
                 TOTAL BORROWING COST
               </div>
-              <div className="flex justify-center items-center w-24 font-bold">
+              <div className="flex justify-start items-center w-24 font-bold text-2xl mr-5">
               ${results?.totalAmountPaid}
               </div>
             </div>
